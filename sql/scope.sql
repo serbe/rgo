37f1b6fdef34d35ca36e58ef897f5fdbb5aaf917
Serbe <mkoctuk@gmail.com>
2021-05-26 11:47:49 +0300

    remove sql

--- sql/scope.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-    scopes (
-        id         bigserial PRIMARY KEY,
-        name       text,
-        note       text,
-        created_at timestamp without time zone,
-        updated_at timestamp without time zone DEFAULT now(),
-        UNIQUE (name)
-    );
